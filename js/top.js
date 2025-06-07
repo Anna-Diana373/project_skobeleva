16b6294bc8655246dd9f729bb5856fe5aca118be
Anna-Diana373 <skobelevaann4@gmail.com>
2025-06-07 12:05:02 +0300

    Add files via upload

--- js/top.js
+++ js/top.js
@@ -1,35 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const slider = document.querySelector('.slider');
-    const slides = document.querySelectorAll('.slide');
-    const prevButton = document.getElementById('prevButton');
-    const nextButton = document.getElementById('nextButton');
-    const slideWidth = slides[0].offsetWidth;
-    let currentPosition = 0;
 
-    function updateSliderPosition() {
-        slider.style.transform = `translateX(${-currentPosition}px)`;
-    }
-
-    function slideNext() {
-        if (currentPosition < slideWidth * (slides.length - 1)) {
-            currentPosition += slideWidth;
-        } else {
-            currentPosition = 0;
-        }
-        updateSliderPosition();
-    }
-
-    function slidePrev() {
-        if (currentPosition > 0) {
-            currentPosition -= slideWidth;
-        } else {
-            currentPosition = slideWidth * (slides.length - 1);
-        }
-        updateSliderPosition();
-    }
-
-    nextButton.addEventListener('click', slideNext);
-    prevButton.addEventListener('click', slidePrev);
-
-    setInterval(slideNext, 5000);
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    preloader.classList.add('fade-out');
+  
+    setTimeout(() => {
+      preloader.remove();
+    }, 500);
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector('.header');
+  var headerHeight = header.offsetHeight + 40;
+  document.body.style.paddingTop = headerHeight + 'px';
 });
